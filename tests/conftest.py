b45eddd106d69e1fb7f8da538b0558fecb424216
klinga <klingaroo@gmail.com>
2025-01-13 10:48:28 -0500

    adds monkey patching for session.send

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,22 +20,22 @@ class FakeUtcNow(datetime.datetime):
 
 
 class MockUnexpectedException:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         raise Exception
 
 
 class MockTimeout:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         raise requests.exceptions.Timeout
 
 
 class MockConnectionError:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         raise requests.exceptions.ConnectionError
 
 
 class MockBookopsSierraError:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         raise BookopsSierraError
 
 
@@ -123,22 +123,25 @@ def mock_token(mock_successful_post_token_response):
 def mock_unexpected_error(monkeypatch):
     monkeypatch.setattr("requests.post", MockUnexpectedException)
     monkeypatch.setattr("requests.Session.get", MockUnexpectedException)
+    monkeypatch.setattr("requests.Session.send", MockUnexpectedException)
 
 
 @pytest.fixture
 def mock_timeout(monkeypatch):
     monkeypatch.setattr("requests.post", MockTimeout)
+    monkeypatch.setattr("requests.get", MockTimeout)
     monkeypatch.setattr("requests.Session.get", MockTimeout)
+    monkeypatch.setattr("requests.Session.send", MockTimeout)
 
 
 @pytest.fixture
-def mock_connectionerror(monkeypatch):
+def mock_connection_error(monkeypatch):
     monkeypatch.setattr("requests.post", MockConnectionError)
     monkeypatch.setattr("requests.Session.get", MockConnectionError)
 
 
 @pytest.fixture
-def mock_bookopssierraerror(monkeypatch):
+def mock_bookopssierra_error(monkeypatch):
     monkeypatch.setattr("requests.Session.get", MockBookopsSierraError)
 
 
