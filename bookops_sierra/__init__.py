669003a27c5d8c021e55e6d1153ab6f53c560683
Charlotte Kostelic <charlottekostelic@gmail.com>
2026-03-05 10:01:02 -0500

    updated package version

--- bookops_sierra/__init__.py
+++ bookops_sierra/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __title__ = "bookops-sierra"
 
 from .authorize import SierraToken  # noqa: F401
-from .session import SierraSession  # noqa: F401
 from .errors import BookopsSierraError  # noqa: F401
+from .session import SierraSession  # noqa: F401
